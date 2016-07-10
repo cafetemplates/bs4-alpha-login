20bf9736c72fc5542263697c55d7b88e52961071
Fernando Moreira <nandomoreira.me@gmail.com>
2016-07-10 20:18:24 -0300

    Compress dist folder

--- tasks/compress.js
+++ tasks/compress.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var size = require('gulp-size');
 var zip = require('gulp-zip');
 
 gulp.task('compress', function() {
-  return gulp.src('dist/*')
+  return gulp.src('dist/**/*')
     .pipe(zip('dist.zip'))
+    .pipe(size({ title: 'Compress dist folder', gzip: false, showFiles: true }))
     .pipe(gulp.dest('./'));
 });
