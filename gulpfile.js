d0e4675eec7d69ac7bad9cb4e60207a4e5d9079e
Fernando Moreira <nandomoreira.me@gmail.com>
2016-07-10 20:11:52 -0300

    Dist files for gh-pages

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('./tasks/sass.js');
-require('./tasks/templates.js');
-require('./tasks/javascripts.js');
-require('./tasks/watch.js');
-require('./tasks/default.js');
-require('./tasks/compress.js');
--- source/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-
-  $(function() {
-    // your code here...
-  });
-
-})(jQuery);
